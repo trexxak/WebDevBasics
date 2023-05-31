390171fdd6cf9287362ba63c8e68467f6576a01f
trexxak <83303245+trexxak@users.noreply.github.com>
2023-05-31 08:41:54 +0200

    .

--- WebDev_Edu/3_intermediate/3_web_apis/2_files/script.js
+++ WebDev_Edu/3_intermediate/3_web_apis/2_files/script.js
@@ -1,71 +1,40 @@
-// Definieren Sie die globale Variable 'fileHandle', die den Dateihandle speichert
+// Globale Variable, die den Dateihandle speichert
 let fileHandle;
 
-// Definieren Sie den Standardmodus. In diesem Fall ist es "x"
-let mode = "x";
-
-// Fügen Sie einen EventListener hinzu, der bei einem Klick auf das Element mit der ID "file_picker" ausgelöst wird
-document.querySelector("#file_picker").onclick = async () => {
-
-    // Öffnen Sie den Datei-Auswahl-Dialog und speichern Sie den ausgewählten Dateihandle in 'fileHandle'
+// Funktion, die beim Klicken auf den "Datei öffnen"-Button ausgelöst wird
+document.querySelector("#openFile").onclick = async () => {
+    // Öffnen des Datei-Auswahl-Dialogs und Speichern des ausgewählten Dateihandles in 'fileHandle'
     [fileHandle] = await window.showOpenFilePicker();
-  
+
     // Abrufen der Datei aus dem Dateihandle
     const file = await fileHandle.getFile();
 
-    // Je nach Modus verschiedene Aktionen ausführen
-    switch (mode) {
+    // Lesen des Dateiinhalts als Text
+    const content = await file.text();
 
-        case "r": // Lesemodus
-            // Dateiinhalt als Text lesen
-            const content = await file.text(); 
+    // Anzeigen des Dateiinhalts im HTML-Element mit der ID "fileContent"
+    document.querySelector("#fileContent").innerText = content;
+}
 
-            // Den gelesenen Textinhalt in das HTML-Element mit der ID "picker_result" schreiben
-            document.getElementById("picker_result").innerHTML = content;
-            break;
-    
-        case "w": // Schreibmodus
-            // Erstellen eines schreibbaren Streams aus dem Dateihandle
-            const writable = await fileHandle.createWritable();
+// Funktion, die beim Klicken auf den "Datei speichern"-Button ausgelöst wird
+document.querySelector("#saveFile").onclick = async () => {
+    // Optionen für den Datei-Speichern-Dialog definieren
+    const options = {
+        types: [{
+            description: "Text files",
+            accept: { "text/plain": [".txt"] },
+        }],
+    };
 
-            // Schreiben eines Textes in den Stream
-            await writable.write("Hallo, dies ist ein Test.");
+    // Öffnen des Datei-Speichern-Dialogs mit den angegebenen Optionen und Speichern des ausgewählten Dateihandles in 'handle'
+    const handle = await window.showSaveFilePicker(options);
 
-            // Schließen des Streams
-            await writable.close();
-            break;
-        
-        case "x": // Speichern als Modus
-            // Optionen für den Datei-Speichern-Dialog definieren
-            const options = {
-                types: [
-                    {
-                        description: "Test files",
-                        accept: {
-                            "text/plain": [".txt"],
-                        },
-                    },
-                ],
-            };
+    // Erstellen eines schreibbaren Streams aus dem neuen Dateihandle
+    const writable = await handle.createWritable();
 
-            // Öffnen des Datei-Speichern-Dialogs mit den angegebenen Optionen und Speichern des ausgewählten Dateihandles in 'handle'
-            const handle = await window.showSaveFilePicker(options);
+    // Schreiben eines Textes in den Stream
+    await writable.write("Hi, ich bin neu hier.");
 
-            // Erstellen eines schreibbaren Streams aus dem Dateihandle
-            const writableHandle = await handle.createWritable();
-    
-            // Schreiben eines Textes in den Stream
-            await writableHandle.write("Hi, ich bin neu hier.");
-
-            // Schließen des Streams
-            await writableHandle.close();
-            break;          
-        
-        // Weitere Modi können hier hinzugefügt werden
-
-        default:
-            console.log("Unbekannter Modus.")
-            break;
-        
-    }
+    // Schließen des Streams
+    await writable.close();
 }
--- WebDev_Edu/3_intermediate/3_web_apis/5_fetch/script.js
+++ WebDev_Edu/3_intermediate/3_web_apis/5_fetch/script.js
@@ -1,6 +1,18 @@
-function useFetch(){
+function fetchTemplate(){
 
     fetch('template.html')
     .then(response => response.text())
     .then(data => document.getElementById("fetchContainer").innerHTML=data);
+}
+
+function fetchPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(posts => console.log(posts[0]["title"]));
+}
+
+function fetchCats() {
+    fetch('https://cat-fact.herokuapp.com/facts/random')
+        .then(response => response.json())
+        .then(catFact => console.log(catFact["text"]));
 }
